d5fa6c0708d6675e314298110ae7de4f7dbf344c
Luke <keatinl1@tcd.ie>
2023-05-13 23:27:14 +0100

    removing unnecessary files

--- controllers/OPP/build/release/OPP.d
+++
@@ -1,6 +0,0 @@
-build/release/OPP.o: OPP.cpp \
- /usr/local/webots/include/controller/cpp/webots/Robot.hpp \
- /usr/local/webots/include/controller/cpp/webots/Motor.hpp \
- /usr/local/webots/include/controller/cpp/webots/Device.hpp \
- /usr/local/webots/include/controller/cpp/webots/../../c/webots/types.h \
- traj.h motor_instructions.h
